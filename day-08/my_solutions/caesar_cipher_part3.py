6f400cd52d92ed75cb1a86fd903bbd7aadb334e1
HC05NP <brian.gabini@ing.com>
2024-07-23 23:31:14 +0800

    feat: add 4 days

---
+++ day-08/my_solutions/caesar_cipher_part3.py
@@ -0,0 +1,18 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
+def caesar(text, shift, direction):
+    result_text = ""
+    if direction == "decode":
+        shift *= -1
+    for letter in text:
+        position = alphabet.index(letter)
+        new_position = position + shift
+        result_text += alphabet[new_position]
+    print(f"Here's the {direction}d result: {result_text}")
+
+caesar(text=text, shift=shift, direction=direction)
